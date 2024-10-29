9660c1926c7e7495eec742feacfab3f0b1a3e920
Michael Werfring <michaelmaximilianw@gmail.com>
2024-10-29 11:49:40 +0100

    Setting Up Driver and Queues

--- worker/app/Program.cs
+++ worker/app/Program.cs
@@ -1,10 +1,61 @@
 ﻿namespace app
 {
+    using lib;
+    using lib.settings;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Options;
+    using RabbitMQ.Client;
+    using System;
+    using System.Text;
+    using static lib.Driver;
+
     public class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        IConfiguration config = hostContext.Configuration;
+
+        services.AddTransient<Driver>();
+        services
+            .AddOptions<QueueServerOptions>()
+            .Bind(config.GetRequiredSection("Driver"));
+    })
+    .Build();
+
+            host.Services
+                .GetRequiredService<Driver>();
+            SetupDummyQueues();
+            host.Run();
+        }
+
+        private static void SetupDummyQueues()
+        {
+            var taskFactory = new ConnectionFactory 
+            { 
+                HostName = "localhost", 
+                Port = 123
+            };
+            using var taskConnection = taskFactory.CreateConnection();
+            using var taskChannel = taskConnection.CreateModel();
+
+            taskChannel.QueueDeclare(queue: "tasks",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            const string message = "Test Task";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            taskChannel.BasicPublish(exchange: string.Empty,
+                                 routingKey: string.Empty,
+                                 basicProperties: null,
+                                 body: body);
         }
     }
 }
--- worker/app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ worker/app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79024ce74dab03401b0f34d94ae235a2c4abf1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2663f2f96b7b881add34741b322bfbcd3fd5d17")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
