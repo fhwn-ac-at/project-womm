d2121594921c5f4ec823eeecc7bc5ed42aa7285d
Michael Werfring <michaelmaximilianw@gmail.com>
2024-11-13 20:58:50 +0100

    Merging feature-split into main

--- worker/app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ worker/app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52856503729068f7f928fbb5574dfd9360facf6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+615e181c7b229dd2ba79f4690390dd7f94880aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
